42f3731b605b25af57ac3643eea13f9d86377bce
evgeniy <ydggs@LAPTOP-GGPT82EI>
2022-06-14 22:05:32 +0300

    added modules calc, even

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,32 +1,9 @@
-import prompt
-from ..cli import welcome_user
-import random
+#!/usr/bin/env python
+from ..calc import calc
 
 
 def main():
-    name = welcome_user()
-    index = 0
-
-    print('What is the result of the expression?')
-
-    while index != 3:
-        first_number = random.randint(1, 100)
-        second_number = random.randint(1, 100)
-        random_symbols = random.choice(['+', '/', '*', '-'])
-        print(f'Question: {first_number} {random_symbols} {second_number}')
-        numbers_sum = eval(f'{first_number}{random_symbols}{second_number}')
-        answer_user = prompt.integer('You answer: ')
-        if numbers_sum == answer_user:
-            print('Correct!')
-            index += 1
-            continue
-        print(f"'{answer_user}' is wrong answer ;(."
-              f"Correct answer was '{numbers_sum}'"
-              f"Let's try again, {name}")
-        break
-
-    if index == 3:
-        print(f'Congratulations, {name}!')
+    calc()
 
 
 if __name__ == '__main__':
