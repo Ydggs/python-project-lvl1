7425cc278cb8e2ee08be8425a8ef42a52cd48af2
evgeniy <ydggs@LAPTOP-GGPT82EI>
2022-06-14 14:24:02 +0300

    three step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+from ..cli import welcome_user
+
 
 def brain_welcome():
-    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print(f'Welcome to the Brain Games! {name}!')
 
 
 if __name__ == '__main__':
