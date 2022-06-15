16a6fc1912f85b100a293b029efe75efe4cee246
evgeniy <ydggs@LAPTOP-GGPT82EI>
2022-06-15 14:13:11 +0300

    added gcd

---
+++ brain_games/gcd.py
@@ -0,0 +1,33 @@
+from .cli import welcome_user
+import prompt
+import random
+
+
+def gcd():
+    name = welcome_user()
+    index = 0
+
+    print('Find the greatest common divisor of given numbers.')
+
+    while index < 3:
+        first_num = random.randint(0, 100)
+        second_num = random.randint(0, 100)
+        greatest_num = max(first_num, second_num)
+
+        print(f'Question: {first_num} {second_num}')
+        answer_user = int(prompt.integer('You answer: '))
+
+        for num in range(greatest_num, 0, -1):
+            if (first_num % num == 0) and (second_num % num == 0)\
+                    and (num == answer_user):
+                index += 1
+                print('Correct!')
+                break
+            elif (first_num % num == 0) and (second_num % num == 0)\
+                    and (num != answer_user):
+                return print(f"'{answer_user}' is wrong answer ;(."
+                             f"Correct answer was '{num} '"
+                             f"Let's try again, {name}")
+
+    if index == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from ..gcd import gcd
+
+
+def main():
+    gcd()
+
+
+if __name__ == '__main__':
+    main()
