6add21398169b454cc8dc939220103152f688930
evgeniy <ydggs@LAPTOP-GGPT82EI>
2022-06-16 22:32:26 +0300

    chainge prime

--- brain_games/prime.py
+++ brain_games/prime.py
@@ -3,36 +3,28 @@ import random
 import prompt
 
 
-def prime():
-    name = welcome_user()
-    index = 0
+def prime(name):
+    random_number = random.randint(2, 100)
+    true_answer = ''
 
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
-    while index < 3:
-        random_number = random.randint(2, 100)
-        true_answer = ''
-
-        for num in range(2, random_number + 1):
-            if random_number % num == 0 and random_number != num:
-                true_answer = 'no'
-                break
-            else:
-                true_answer = 'yes'
-
-        print(f'Question: {random_number}', true_answer)
-        user_answer = ''
-
-        while user_answer != 'yes' and user_answer != 'no':
-            user_answer = prompt.string('You answer: ')
-
-        if user_answer == true_answer:
-            print('Correct!')
-            index += 1
-            continue
+    for num in range(2, random_number + 1):
+        if random_number % num == 0 and random_number != num:
+            true_answer = 'no'
         else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{true_answer} '")
-            return print(f"Let's try again, {name}!")
+            true_answer = 'yes'
 
-    return print(f'Congratulations, {name}!')
+    print(f'Question: {random_number}', true_answer)
+    user_answer = ''
+
+    while user_answer != 'yes' and user_answer != 'no':
+        user_answer = prompt.string('You answer: ')
+
+    if user_answer == true_answer:
+        print('Correct!')
+    else:
+        print(f"'{user_answer}' is wrong answer ;(."
+              f"Correct answer was '{true_answer} '")
+        return print(f"Let's try again, {name}!")
+
